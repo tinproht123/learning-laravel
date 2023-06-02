79bd7a10587856416d513f4885c0ff5303f21c79
Minh Anh <tinproht123@gmail.com>
2023-06-02 10:38:56 +0700

    add new template for project

--- practice-e-comerce-app/app/Http/Controllers/Admin/DashboardController.php
+++ practice-e-comerce-app/app/Http/Controllers/Admin/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function Index(){
-        return view('userprofile');
+        return view('admin.dashboard');
     }
 }
